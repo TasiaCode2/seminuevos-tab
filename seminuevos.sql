9eef6791145af11216b78945efa7e6db56cc4ce4
TasiaCode2 <49112029+TasiaCode2@users.noreply.github.com>
2024-03-27 12:19:45 -0600

    Agregar script de la base de datos

---
+++ seminuevos.sql
@@ -0,0 +1,162 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-03-2024 a las 19:11:21
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `seminuevos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `anuncio`
+--
+
+CREATE TABLE `anuncio` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(30) NOT NULL,
+  `precio` float NOT NULL,
+  `kilometraje` int(11) NOT NULL,
+  `categoria` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` tinyint(4) NOT NULL,
+  `tipo` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `tipo`) VALUES
+(2, 'Sedan'),
+(3, 'Hatchback'),
+(4, 'SUV'),
+(5, 'Pickup'),
+(6, 'Moto'),
+(7, 'Coupé');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `link_foto`
+--
+
+CREATE TABLE `link_foto` (
+  `id` int(11) NOT NULL,
+  `anuncio_id` int(11) NOT NULL,
+  `link` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `login`
+--
+
+CREATE TABLE `login` (
+  `id` tinyint(4) NOT NULL,
+  `usuario` varchar(10) NOT NULL,
+  `contra` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `anuncio`
+--
+ALTER TABLE `anuncio`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria` (`categoria`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `link_foto`
+--
+ALTER TABLE `link_foto`
+  ADD PRIMARY KEY (`id`,`anuncio_id`),
+  ADD KEY `anuncio_id` (`anuncio_id`);
+
+--
+-- Indices de la tabla `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `anuncio`
+--
+ALTER TABLE `anuncio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `link_foto`
+--
+ALTER TABLE `link_foto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `anuncio`
+--
+ALTER TABLE `anuncio`
+  ADD CONSTRAINT `anuncio_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id`);
+
+--
+-- Filtros para la tabla `link_foto`
+--
+ALTER TABLE `link_foto`
+  ADD CONSTRAINT `link_foto_ibfk_1` FOREIGN KEY (`anuncio_id`) REFERENCES `anuncio` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
